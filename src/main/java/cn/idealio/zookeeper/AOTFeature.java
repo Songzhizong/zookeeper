f67a0184e6dd7ea8382adce6f192efae01a4eb61
宋志宗 <zzsong91@163.com>
2023-09-17 22:25:46 +0800

    支持Prometheus监控 close #3

--- src/main/java/cn/idealio/zookeeper/AOTFeature.java
+++ src/main/java/cn/idealio/zookeeper/AOTFeature.java
@@ -1,7 +1,11 @@
 package cn.idealio.zookeeper;
 
+import jline.console.completer.CandidateListCompletionHandler;
 import org.graalvm.nativeimage.hosted.Feature;
+import org.graalvm.nativeimage.hosted.RuntimeJNIAccess;
 import org.graalvm.nativeimage.hosted.RuntimeResourceAccess;
+
+import javax.servlet.Servlet;
 
 /**
  * @author 宋志宗 on 2023/9/13
@@ -11,12 +15,46 @@ public class AOTFeature implements Feature {
 
     @Override
     public void duringSetup(DuringSetupAccess access) {
-        Module module = AOTFeature.class.getModule();
-        RuntimeResourceAccess.addResource(module, "logback.xml");
+        try {
+            Module module = AOTFeature.class.getModule();
+            RuntimeResourceAccess.addResource(module, "logback.xml");
 
-        RuntimeResourceAccess.addResource(
-                jline.console.completer.CandidateListCompletionHandler.class.getModule(),
-                "jline/console/completer/CandidateListCompletionHandler.properties"
-        );
+            Module jlineModule = CandidateListCompletionHandler.class.getModule();
+            RuntimeResourceAccess.addResource(
+                    jlineModule,
+                    "jline/console/completer/CandidateListCompletionHandler.properties"
+            );
+
+
+            Module servletModule = Servlet.class.getModule();
+            RuntimeResourceAccess.addResource(servletModule, "javax/servlet/LocalStrings_fr.properties");
+            RuntimeResourceAccess.addResource(servletModule, "javax/servlet/LocalStrings_ja.properties");
+            RuntimeResourceAccess.addResource(servletModule, "javax/servlet/LocalStrings.properties");
+            RuntimeResourceAccess.addResource(servletModule, "javax/servlet/http/LocalStrings_es.properties");
+            RuntimeResourceAccess.addResource(servletModule, "javax/servlet/http/LocalStrings_fr.properties");
+            RuntimeResourceAccess.addResource(servletModule, "javax/servlet/http/LocalStrings_ja.properties");
+            RuntimeResourceAccess.addResource(servletModule, "javax/servlet/http/LocalStrings.properties");
+
+            Class<?> OperatingSystemImplClass = Class.forName("com.sun.management.internal.OperatingSystemImpl");
+            RuntimeJNIAccess.register(OperatingSystemImplClass);
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getCommittedVirtualMemorySize0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getFreeMemorySize0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getFreeSwapSpaceSize0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getMaxFileDescriptorCount0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getOpenFileDescriptorCount0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getProcessCpuTime0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getProcessCpuLoad0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getCpuLoad0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getTotalMemorySize0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getTotalSwapSpaceSize0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getSingleCpuLoad0", int.class));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getHostConfiguredCpuCount0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getHostOnlineCpuCount0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("getHostTotalCpuTicks0"));
+            RuntimeJNIAccess.register(OperatingSystemImplClass.getDeclaredMethod("initialize0"));
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
